f5ab4984f56c77f73059047922bbad05d477623e
NiniGogidze <119741770+NiniGogidze@users.noreply.github.com>
2023-01-19 23:10:07 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,33 @@
+// 1
+let person = 
+    {
+      name: "nini",
+      lastName: "gogidze",
+      age: 17,
+      address: "gldani",
+    }
+
+console.log(`my name is ${person.name}`);
+console.log(`my age is ${person.age}`);
+console.log(`my address is ${person.address}`);
+
+
+// 3
+
+let age = 17;
+if (age > 19) {
+  console.log("more than 19");
+} else if (age <= 19) {
+  console.log("I am a teenager");
+} else {
+  console.log("I am an adult");
+}
+
+// 4
+const num = [`1`, `2`, `3`, `4`,`5`]
+
+console.log(num[0]);
+console.log(num[1]);
+console.log(num[2]);
+console.log(num[3]);
+console.log(num[4]);
